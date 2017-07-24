8e44f1537a040974876b8ffa7914ae1741f19e00
vinoth <vinoth@oxsoftwares.com>
2017-07-24 16:00:25 +0530

    Buttons added in index page

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
@@ -6,12 +5,20 @@ and open the template in the editor.
 -->
 <html>
     <head>
+        <title>Register</title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id="main1">
+            <div id="header">
+                <div id="title">
+                    <h1><i><b>WELCOME</b></i></h1></div></div>
+            <div id="btns">
+                <a href="login.php"><input type="submit" id="sign_bt" value="Sign In"></a>
+                <a href="register.php"><input type="submit" id="reg_bt" value="Register"></a>
+            </div>
+        </div>
     </body>
 </html>
