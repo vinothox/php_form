b2c05a8c87e31fc7994a65fca01b3d319b124044
vinoth <vinoth@oxsoftwares.com>
2017-07-25 17:53:37 +0530

    added table to display db in index.php

--- index.php
+++ index.php
@@ -19,6 +19,32 @@ and open the template in the editor.
                 <a href="login.php"><input type="submit" id="sign_bt" value="Sign In"></a>
                 <a href="register.php"><input type="submit" id="reg_bt" value="Register"></a>
             </div>
+            <div id="tb">
+                <h2>Our Registered Users</h2>
+            <?php
+            $db = mysqli_connect('localhost', 'root', 'admin@123', 'account');
+            $query = "SELECT username,email,city FROM users";
+            $result = mysqli_query($db, $query);
+
+            echo "<table border='1' width='100%' border-collapse='collapse'>
+                <tr>
+                <th>Username</th>
+                <th>Email</th>
+                <th>City</th>
+                </tr>";
+
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['username'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
+                echo "<td>" . $row['city'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+
+            mysqli_close($con);
+            ?>
+        </div>
         </div>
     </body>
 </html>
